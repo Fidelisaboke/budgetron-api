333349b0896212283c5549cddb4ccbb379dde50e
Fidelisaboke <fidelisaboke@gmail.com>
2025-07-18 19:47:05 +0300

    chore: production setup

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from budgetron import create_app
-
+import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    debug_mode = os.getenv('DEBUG_MODE', 'False').lower() == 'true'
+    app.run(debug=debug_mode)
